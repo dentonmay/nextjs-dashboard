3dc3fdf6e6c12ce570ae2387a23cfc684e9b552c
dentonmay <dentondmay@gmail.com>
2025-02-10 17:17:49 -0700

    second commit

--- auth.config.js
+++ auth.config.js
@@ -2,7 +2,7 @@ export const authConfig = {
     pages: {
       signIn: '/login',
     },
-
+  
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
